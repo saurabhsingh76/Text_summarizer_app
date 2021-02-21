44654f4e690623888bd0ddd6364f4163fc94f595
Shubham Kaushik <shubhdeep2226k@gmail.com>
2021-02-21 13:19:02 +0530

    added code for upload image and ocr in app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 import time
-from flask import Flask, render_template, request,url_for
+import cv2
+import pytesseract
+from flask import Flask, render_template, request, url_for
+from PIL import Image
 
 from summarizer1 import summarize
 
+UPLOAD_FOLDER = '/static/uploads/'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -13,7 +20,7 @@ def index():
 
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
-    global raw_text
+    global rawtext
     start = time.time()
     if request.method == 'POST':
         rawtext = request.form['rawtext']
@@ -28,6 +35,40 @@ def analyze():
     return render_template('index.html', ctext=rawtext, final_summary=final_summary, final_summary_spacy=final_summary,
                            final_summary_gensim=final_summary_gensim, final_summary_nltk=final_summary_nltk,
                            final_summary_sumy=final_summary_sumy)
+
+
+@app.route('/ocr', methods=['POST', 'GET'])
+def upload_file():
+    start = time.time()
+    if request.method == "POST":
+        file = request.files['file']
+
+        f = os.path.join(app.config['UPLOAD_FOLDER'],'ocr_image.jpg')
+
+        # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+        file.save(f)
+        # print(file.filename)
+
+        image = cv2.imread(UPLOAD_FOLDER + "/ocr_image.jpg")
+        os.remove(UPLOAD_FOLDER + "ocr_image.jpg")
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        rawtext = pytesseract.image_to_string(Image.open('ocr_image.jpg'))
+
+        final_summary = summarize(rawtext)
+        final_summary_gensim = summarize(rawtext)
+        # NLTK
+        final_summary_nltk = summarize(rawtext)
+        # Sumy
+        final_summary_sumy = summarize(rawtext)
+        end = time.time()
+        final_time = end - start
+        return render_template('index.html', ctext=rawtext, final_summary=final_summary,
+                               final_summary_spacy=final_summary,
+                               final_summary_gensim=final_summary_gensim, final_summary_nltk=final_summary_nltk,
+                               final_summary_sumy=final_summary_sumy)
+
+        # os.remove('ocr_image.jpg')
 
 
 app.run(debug=True)
